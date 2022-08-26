eb98bd672b2aa0dc19a69228a7d3d3fda8d33ae7
Shiv25z <shivamzagade25899@gmail.com>
2022-08-26 20:37:49 +0530

    Second commit

--- flipcoinSimulator.sh
+++ flipcoinSimulator.sh
@@ -1,13 +1,27 @@
-#!/bin/bash
+0#!/bin/bash
 
 head_count=0
 tail_count=0
+no_of_flips=0
+target=0
 
-flipcoin=$((RANDOM%2))
-if [ $flipcoin -eq 1 ]
-then 
-	echo "Heads"
+while (( 1 ))
+do	
+	flipcoin=$((RANDOM%2))
+	(( no_of_flips++ )) 
+	echo "Number of flips: $no_of_flips"
 	
-else
-	echo "Tails"
-fi
+	if [ $flipcoin -eq 1 ]
+	then 
+		echo "Heads"
+		(( head_count++ ))
+		echo $head_count
+		
+	else
+		echo "Tails"
+		(( tail_count++))
+		echo $tail_count
+	fi
+	
+
+done
