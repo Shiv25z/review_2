42cfd4be650ea36a298c38cfc64ef9177532525e
Shiv25z <shivamzagade25899@gmail.com>
2022-08-26 21:07:11 +0530

    Third commit

--- flipcoinSimulator.sh
+++ flipcoinSimulator.sh
@@ -1,9 +1,11 @@
-0#!/bin/bash
+#!/bin/bash
 
 head_count=0
 tail_count=0
 no_of_flips=0
-target=0
+target=21
+min_difference=2
+
 
 while (( 1 ))
 do	
@@ -11,7 +13,7 @@ do
 	(( no_of_flips++ )) 
 	echo "Number of flips: $no_of_flips"
 	
-	if [ $flipcoin -eq 1 ]
+	if (( $flipcoin == 1 ))
 	then 
 		echo "Heads"
 		(( head_count++ ))
@@ -23,5 +25,20 @@ do
 		echo $tail_count
 	fi
 	
+	differnce=$(( head_count - tail_count))
+	if (( head_count == target && $difference >= min_difference ))
+	then
+		echo "Heads won "
+		break;
+	
+	elif (( tail_count == target && $difference >= min_difference ))
+	then 		
+		echo "Tails won"
+		break;
+	elif (( head_count == tail_count ))
+	then
+		echo "Its a Tie! Starting Again"
+	
+	fi
 
 done
